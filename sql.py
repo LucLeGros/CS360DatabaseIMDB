2391c5903f177d086e656e6bd116365fa864cae7
Nick-Bosworth <nicholasbosworth014@gmail.com>
2018-12-01 12:05:14 +0900

    Updated Insert
    Speed is much better
    Small schema issue on last test but I'm checking now to see what went wrong (apparently there was a violation of a unique constraint)

--- sql.py
+++ sql.py
@@ -36,78 +36,65 @@ def init_db():
         db.cursor().executescript(f.read())
     db.commit()
 
-def insert_into_db (file, data):
-    db = get_db()
+def insert_into_db (file, data, db):
     if file == "basics.tsv":
         #Insert into media table
         db.cursor().execute('INSERT INTO media VALUES (?, ?, ?, ?, ?, ?, ?,?,?,?)',\
         (data.get("tconst"), data.get("titleType"), data.get("primaryTitle"),\
          data.get("originalTitle"), data.get("isAdult"), data.get("startYear"),\
          data.get("endYear"), data.get("runtimeMinutes"), None, None))
-        db.commit()
         #Insert into genres table
         if data.get("genres") is not None:
             for genre in data.get("genres").split(","):
                 db.cursor().execute ("INSERT INTO genres VALUES (?, ?)", \
                 (data.get("tconst"), genre))
-                db.commit()
     elif file == "ratings.tsv":
         #Insert into media table
         db.cursor().execute('UPDATE media SET "average_rating" = ?, "number_of_votes" = ? WHERE "tconst" = ?',\
         (data.get("averageRating"), data.get("numVotes"), data.get("tconst")))
-        db.commit()
     elif file == "akas.tsv":
         #Insert into alternate_titles table
         db.cursor().execute('INSERT INTO alternate_titles VALUES (?, ?, ?, ?, ?, ?, ?, ?)',\
         (data.get("titleId"), data.get("ordering"), data.get("title"), data.get("region"),\
          data.get("languages"), data.get("types"), data.get("attributes"), \
          data.get("isOriginalTitle")))
-        db.commit()
     elif file == "episode.tsv":
         #Insert into episodes table
         db.cursor().execute('INSERT INTO episodes VALUES (?, ?, ?, ?)',\
         (data.get("tconst"), data.get("parentTconst"), data.get("seasonNumber"),\
          data.get("episodeNumber")))
-        db.commit()
     elif file == "principals.tsv":
         #Insert into crew, characters tables
         #Insert into crew
         db.cursor().execute('INSERT INTO crew VALUES (?, ?, ?, ?, ?, ?)', \
         (data.get("tconst"), data.get("ordering"), data.get("nconst"),\
         data.get("category"), data.get("job"), False))
-        db.commit()
         #Insert into characters
         if data.get("characters") is not None:
             for role in eval(data.get("characters")):
                 db.cursor().execute('INSERT INTO characters VALUES (?, ?, ?)',\
                 (data.get("tconst"), data.get("nconst"), role))
-                db.commit()
     elif file == "name.tsv":
         #Insert into person, crew, professions tables
         #Insert into person
         db.cursor().execute('INSERT INTO person VALUES (?, ?, ?, ?)',\
         (data.get("nconst"), data.get("primaryName"), data.get("birthYear"),\
         data.get("deathYear")))
-        db.commit()
         #Insert into professions table (for professions composite attribute)
         for job in data.get("primaryProfession").split(","):
             db.cursor().execute('INSERT INTO professions VALUES (?, ?)',\
             (data.get("nconst"), job))
-            db.commit()
         #Update crew table to reflect roles person is known for
         for title in data.get("knownForTitles").split(","):
             db.cursor().execute('UPDATE crew SET known_for = ? WHERE tconst = ?', (True, title))
-            db.commit()
 
-def populate_db():
-    load_limit = get_entities()
+def populate_db ():
+    db = get_db()
     for table in get_data():
-        entities = load_limit
         with open (os.path.join('data', table)) as tsvfile:
-            for row in csv.DictReader(tsvfile, dialect='excel-tab'):
-                if entities == 0: break
+            for row in csv.DictReader(tsvfile, dialect='excel-tab', quoting=csv.QUOTE_NONE):
                 for key in row:
                     if row[key] == "\\N":
                         row[key] = None
-                insert_into_db (table, row)
-                entities -= 1
+                insert_into_db (table, row, db)
+        db.commit()
