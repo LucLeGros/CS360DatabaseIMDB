168586b023892abfc8164f0f7142efe11a7ad8d5
LucLeGros <3gmichels@gmail.com>
2018-12-05 15:51:36 +0900

    added some functions
    getcolor
    computemapfilmdensity

--- Public/main.js
+++ Public/main.js
@@ -12,23 +12,37 @@ $(".container").mapael({
 
 function onclick(e, id, mapElem, textElem) {
 	    var newData = {
-            'areas': {}
+            'areas': $(".container").data('mapael').areas
         };
-        if (mapElem.originalAttrs.fill == "#5ba4ff") {
-            newData.areas[id] = {
-                attrs: {
-                    fill: "#0088db"
-                }
-            };
-        } else {
-            newData.areas[id] = {
-                attrs: {
-                    fill: "#5ba4ff"
-                }
-            };
-        }
+        Object.keys(newData.areas).forEach(key => {
+            var value = newData.areas[key];
+             value.attrs = {fill: "#000088"}
+        });
+        
+
+        
         $(".container").trigger('update', [{mapOptions: newData}]);
 }
+
+function computeMapFilmDensity(map){
+    var newData = {
+            'areas': $(".container").data('mapael').areas
+        };
+    Object.keys(newData.areas).forEach(key => {
+            var value = newData.areas[key];
+            value.attrs = {fill: "#880000"}
+        });
+    $(".container").trigger('update', [{mapOptions: newData}]);
+}
+window.onload = computeMapFilmDensity 
+
+function getColor(numberOfFilms,maxFilms){//we will assume the min film is 0 which is probably the case
+    var blue = "ff";
+    var redGreenGradient = Math.round(numberOfFilms/maxFilms)
+    return "" + redGreenGradient + redGreenGradient + blue//"" at the begining so we get "1111ff" and not "22ff" 
+}
+
+
 
 
 
