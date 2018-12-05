07732023e92c12a830c6f2143c851c0a82a8e230
LucLeGros <3gmichels@gmail.com>
2018-12-05 13:06:46 +0900

    hello

--- Public/main.js
+++ Public/main.js
@@ -1,5 +1,34 @@
 $(".container").mapael({
     map : {
-        name : "world_countries"
+        name : "world_countries",
+
+        defaultArea: {
+	        eventHandlers: {
+	                            click: onclick
+	                        }
+        }
     }
 });
+
+function onclick(e, id, mapElem, textElem) {
+	    var newData = {
+            'areas': {}
+        };
+        if (mapElem.originalAttrs.fill == "#5ba4ff") {
+            newData.areas[id] = {
+                attrs: {
+                    fill: "#0088db"
+                }
+            };
+        } else {
+            newData.areas[id] = {
+                attrs: {
+                    fill: "#5ba4ff"
+                }
+            };
+        }
+        $(".container").trigger('update', [{mapOptions: newData}]);
+}
+
+
+
