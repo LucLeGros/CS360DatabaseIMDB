545990faf957e980c7889e1b637a14511499e2c2
LucLeGros <3gmichels@gmail.com>
2018-12-08 15:46:39 +0900

    added clause

--- Public/main.js
+++ Public/main.js
@@ -1,3 +1,8 @@
+var newData
+var mapCountryFilmNumber = new Map()//map[myKey1] = myObj1;
+var filmDescription = []
+
+
 $(".container").mapael({
     map : {
         name : "world_countries",
@@ -10,30 +15,43 @@ $(".container").mapael({
     }
 });
 
-function onclick(e, id, mapElem, textElem) {
-	    //getMapFilmDensity().then()
-        
 
+window.onload = colorWorldMap 
+
+function colorWorldMap(){
+    getMapFilmDensity().then(computeMapFilmDensity)//map has been computed     
+}
+function onclickFindFilmsDescription(e, id, mapElem, textElem) {
+	getFilmForCountry(id).then(populate(filmDescription))//filmDescription has been computed
         
-        $(".container").trigger('update', [{mapOptions: newData}]);
 }
 
-function computeMapFilmDensity(map){
-    var newData = {
+function computeMapFilmDensity(){
+    newData = {
             'areas': $(".container").data('mapael').areas
         };
-    var filmArayNumber = getMapFilmDensity()
+    var maxFilms = Math.max(mapCountryFilmNumber.values())
     //var maxFilms = 
+    console.log(newData.areas)
     Object.keys(newData.areas).forEach(key => {
-            var value = newData.areas[key];
-            value.attrs = {fill: getColor()}
+            var country = newData.areas[key];//finding what is the country key might not be this I can't remember
+
+            var numberOfFilms = mapCountryFilmNumber.has(key) ? mapCountryFilmNumber.get(key) : 0
+
+            //compute color of one country
+            console.log(numberOfFilms+  "nb")
+            console.log(getColor(numberOfFilms,maxFilms) + "color")
+            country.attrs = {fill: getColor(numberOfFilms,maxFilms)}
         });
     $(".container").trigger('update', [{mapOptions: newData}]);
 }
-//window.onload = computeMapFilmDensity 
+
 
 //creates color that ranges from white to blue (blue is high film density)
 function getColor(numberOfFilms,maxFilms){//we will assume the min film is 0 which is probably the case
+    if(maxFilms == 0){
+        return "#FFFFff"
+    }
     var blue = "ff";
     var redGreenGradient = (Math.round((maxFilms-numberOfFilms)/maxFilms)*15).toString(16)//0 to f
 
@@ -42,10 +60,29 @@ function getColor(numberOfFilms,maxFilms){//we will assume the min film is 0 whi
 
 function getMapFilmDensity(){
 
+    return API.getFilmsPerCountry().then(result => {
+        console.log(result)
+        console.log(typeof(result))
+        for (var i = 0; i < result.length; ++i){
+            var elem = result[i]
+            var k = elem["region"]
+
+            var v = elem["COUNT(*)"]
+            //add to our map so we can ignore weird countries
+            console.log(k + " key region " + v + " count film")
+            mapCountryFilmNumber.set(k, v)
+        }
+        
+    })
 }
 
-function getFilmForCountry(){
-
+function getFilmForCountry(id){//i don't know what form the result will be in 
+    filmDescription = []
+    API.getFilmsInCountry(id).then(result =>
+            Object.keys(result).forEach(key => 
+                filmDescription.push(key.title)
+                )
+        )
 }
 
 function populate(filmsForCountry){
